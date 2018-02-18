72b0c17bbaceeca5bcadb670e5addc4cff3e4cb9
Curtis <curtis@drumeo.com>
2018-02-17 18:40:50 -0800

    Small formatting fixes. Added in one level of specificity to the css.

--- src/components/MediaElement.vue
+++ src/components/MediaElement.vue
@@ -3,8 +3,7 @@
         <video :poster="poster"
                :id="playerId"
                class="mejs__player">
-
-
+            
             <source v-for="(source, i) in sources"
                     :key="i"
                     :src="source.file"
@@ -83,6 +82,7 @@
                 skipBackInterval: 10,
                 speeds: ['0.5', '0.75', '1.00', '1.25', '1.5'],
                 timeAndDurationSeparator: ' / ',
+                qualityText: 'Video Quality',
                 success: (mediaElement) => {
                     vm.addMediaElementEventListeners(mediaElement);
                 },
@@ -96,131 +96,133 @@
 
 <style lang="scss">
 
-    .mejs__time-rail {
-        position:absolute;
-        bottom:2em;
-        left:0;
-        right:0;
-        width:100%;
-        box-sizing:border-box;
-        padding:1em 1em 0;
-        margin:0;
+    .media-element {
+        .mejs__time-rail {
+            position:absolute;
+            bottom:2em;
+            left:0;
+            right:0;
+            width:100%;
+            box-sizing:border-box;
+            padding:1em 1em 0;
+            margin:0;
 
-        .mejs__time-total {
-            width:calc(100% - 2em);
-            height:6px;
-            margin-top:0;
-        }
-        
-        .mejs__time-current {
-            background:#0b76db;
-        }
-
-        .mejs__time-buffering, .mejs__time-current, .mejs__time-hovered, .mejs__time-loaded {
-            height:6px;
-        }
-
-        .mejs__time-handle, .mejs__time-handle-content {
-            display:none;
-        }
-    }
-
-    .mejs__skip-back-button {
-        position:absolute;
-        bottom:5em;
-        left:1em;
-    }
-
-    .mejs__jump-forward-button {
-        position:absolute;
-        bottom:5em;
-        right:1em;
-    }
-
-    .mejs__playpause-button {
-        position:absolute;
-        bottom:0;
-        left:1em;
-    }
-
-    .mejs__volume-button {
-        position:absolute;
-        bottom:0;
-        left:5em;
-    }
-
-    .mejs__time {
-        position:absolute;
-        bottom:0;
-        left:8.5em;
-    }
-
-    .mejs__fullscreen-button {
-        position:absolute;
-        bottom:0;
-        right:1em;
-    }
-
-    .mejs__speed-button {
-        position:absolute;
-        bottom:0;
-        right:5.5em;
-
-        button {
-            font-weight:700;
-            font-family:'Open Sans', sans-serif;
-        }
-
-        .mejs__speed-selector {
-            height:125px;
-
-            .mejs__speed-selector-list-item {
-                margin:0;
-                padding:3px;
-
-                &:hover {
-                    background-color: rgba(255, 255, 255, 0.4) !important;
-                }
+            .mejs__time-total {
+                width:calc(100% - 2em);
+                height:6px;
+                margin-top:0;
             }
-            .mejs__speed-selector-label {
+
+            .mejs__time-current {
+                background:#0b76db;
+            }
+
+            .mejs__time-buffering, .mejs__time-current, .mejs__time-hovered, .mejs__time-loaded {
+                height:6px;
+            }
+
+            .mejs__time-handle, .mejs__time-handle-content {
+                display:none;
+            }
+        }
+
+        .mejs__skip-back-button {
+            position:absolute;
+            bottom:5em;
+            left:1em;
+        }
+
+        .mejs__jump-forward-button {
+            position:absolute;
+            bottom:5em;
+            right:1em;
+        }
+
+        .mejs__playpause-button {
+            position:absolute;
+            bottom:0;
+            left:1em;
+        }
+
+        .mejs__volume-button {
+            position:absolute;
+            bottom:0;
+            left:5em;
+        }
+
+        .mejs__time {
+            position:absolute;
+            bottom:0;
+            left:8.5em;
+        }
+
+        .mejs__fullscreen-button {
+            position:absolute;
+            bottom:0;
+            right:1em;
+        }
+
+        .mejs__speed-button {
+            position:absolute;
+            bottom:0;
+            right:5.5em;
+
+            button {
                 font-weight:700;
                 font-family:'Open Sans', sans-serif;
-                margin-left:5px;
+            }
 
-                &.mejs__speed-selected {
-                    color:#0b76db;
+            .mejs__speed-selector {
+                height:125px;
+
+                .mejs__speed-selector-list-item {
+                    margin:0;
+                    padding:3px;
+
+                    &:hover {
+                        background-color: rgba(255, 255, 255, 0.4) !important;
+                    }
+                }
+                .mejs__speed-selector-label {
+                    font-weight:700;
+                    font-family:'Open Sans', sans-serif;
+                    margin-left:5px;
+
+                    &.mejs__speed-selected {
+                        color:#0b76db;
+                    }
                 }
             }
         }
-    }
 
-    .mejs__qualities-button {
-        position:absolute;
-        bottom:0;
-        right:10em;
+        .mejs__qualities-button {
+            position:absolute;
+            bottom:0;
+            right:10em;
 
-        button {
-            font-weight:700;
-            font-family:'Open Sans', sans-serif;
-        }
-
-        .mejs__qualities-selector {
-
-            .mejs__qualities-selector-list-item {
-                margin:0;
-                padding:3px;
-
-                &:hover {
-                    background-color: rgba(255, 255, 255, 0.4) !important;
-                }
-            }
-            .mejs__qualities-selector-label {
+            button {
                 font-weight:700;
                 font-family:'Open Sans', sans-serif;
-                margin-left:5px;
+            }
 
-                &.mejs__qualities-selected {
-                    color:#0b76db;
+            .mejs__qualities-selector {
+
+                .mejs__qualities-selector-list-item {
+                    margin:0;
+                    padding:3px;
+
+                    &:hover {
+                        background-color: rgba(255, 255, 255, 0.4) !important;
+                    }
+                }
+                .mejs__qualities-selector-label {
+                    font-weight:700;
+                    font-family:'Open Sans', sans-serif;
+                    margin-left:5px;
+
+                    &.mejs__qualities-selected {
+                        color:#0b76db;
+                    }
                 }
             }
         }
