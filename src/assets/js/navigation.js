e2c1b53ee26f267f5e6c38bed77e46483f0bfa63
Curtis <curtis@drumeo.com>
2018-04-16 15:59:19 -0700

    Added a condition to the nav.js incase the top bar doesn't exist.

--- src/assets/js/navigation.js
+++ src/assets/js/navigation.js
@@ -2,21 +2,23 @@ export default (function(){
     const menuButton = document.getElementById('menuButton');
     const navSideBar = document.getElementById('navSideBar');
 
-    menuButton.addEventListener('click', event => {
-        event.stopPropagation();
+    if(menuButton){
+        menuButton.addEventListener('click', event => {
+            event.stopPropagation();
 
-        menuButton.classList.toggle('active');
-        navSideBar.classList.toggle('active');
-    });
+            menuButton.classList.toggle('active');
+            navSideBar.classList.toggle('active');
+        });
 
-    navSideBar.addEventListener('click', event => {
-        event.stopPropagation();
-    });
+        navSideBar.addEventListener('click', event => {
+            event.stopPropagation();
+        });
 
-    document.addEventListener('click', event => {
-        event.stopPropagation();
+        document.addEventListener('click', event => {
+            event.stopPropagation();
 
-        menuButton.classList.remove('active');
-        navSideBar.classList.remove('active');
-    });
+            menuButton.classList.remove('active');
+            navSideBar.classList.remove('active');
+        });
+    }
 })();
