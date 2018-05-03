32302a4ec99ca5dcf4d984819060c8b6566637dc
Curtis <curtis@drumeo.com>
2018-05-03 08:08:52 -0700

    Weird.

---
+++ src/views/forum/ForumThread.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container mv-3 forum-post">
+        <div class="flex flex-column bg-white shadow corners-3">
+            <div class="flex flex-row pa bb-light-1">
+                <h1 class="heading">
+                    <a href="" class="no-decoration mr-1">
+                        <i class="fas fa-arrow-circle-left text-light"></i>
+                    </a>
+
+                    This is a Forum Title
+                </h1>
+            </div>
+            <div class="flex flex-row">
+                <div class="flex flex-column pa">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'forum-thread'
+    }
+</script>
+<style>
+
+</style>
