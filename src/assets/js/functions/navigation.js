8a97d97ab28530deaadc35af5d16128d8ee5e999
Curtis <curtis@drumeo.com>
2018-07-17 13:49:18 -0700

    Added in support for multi level nav items.

--- src/assets/js/functions/navigation.js
+++ src/assets/js/functions/navigation.js
@@ -1,6 +1,10 @@
 export default (function(){
     const menuButton = document.getElementById('menuButton');
     const navSideBar = document.getElementById('navSideBar');
+    const searchIcon = document.getElementById('searchIcon');
+    const searchBox = document.getElementById('searchBox');
+    const searchInput = document.getElementById('searchInput');
+    const parentLinks = document.querySelectorAll('.parent-button');
     let backgroundOverlay = document.getElementById('backgroundOverlay');
 
     if(menuButton){
@@ -9,6 +13,16 @@ export default (function(){
 
         navSideBar.addEventListener('click', event => {
             event.stopPropagation();
+        });
+    }
+
+    if(searchIcon){
+        searchIcon.addEventListener('click', toggleSearchBar);
+    }
+
+    if(parentLinks.length){
+        Array.from(parentLinks).forEach(link => {
+            link.addEventListener('click', toggleChildLinks);
         });
     }
 
@@ -24,6 +38,35 @@ export default (function(){
             menuButton.classList.add('active');
             navSideBar.classList.add('active');
             backgroundOverlay.classList.add('active');
+        }
+    }
+
+    function toggleSearchBar(event){
+        event.stopPropagation();
+
+        searchBox.classList.toggle('active');
+
+        if(searchBox.classList.contains('active')){
+            searchInput.focus();
+        }
+        else {
+            searchInput.blur();
+        }
+    }
+
+    function toggleChildLinks(event){
+        event.stopPropagation();
+
+        const buttonClicked = event.target.parentElement;
+        const childLinks = buttonClicked.children[1];
+
+        event.target.classList.toggle('active');
+
+        if(event.target.classList.contains('active')){
+            childLinks.style['max-height'] = childLinks.scrollHeight + 'px';
+        }
+        else {
+            childLinks.removeAttribute('style');
         }
     }
 })();
