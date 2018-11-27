9496703feb86e5c1ed3492e8c50abae121b1bfb2
Curtis <curtis@drumeo.com>
2018-11-27 10:08:28 -0800

    Default comment sorting is now popularity.

--- src/views/comments/Comments.vue
+++ src/views/comments/Comments.vue
@@ -8,9 +8,9 @@
             <div class="flex flex-column xs-12 sm-4 md-3 mb-3">
                 <div class="form-group xs-12" style="width:100%;">
                     <select id="commentSort" v-model="sortInterface">
+                        <option value="-like_count">Popularity</option>
                         <option value="-created_on">Latest First</option>
                         <option value="created_on">Oldest First</option>
-                        <option value="-like_count">Popular</option>
                         <option value="-mine">My Comments</option>
                     </select>
                     <label for="commentSort" :class="themeColor">Sort By</label>
@@ -158,7 +158,7 @@
                 comments: [],
                 pinnedComment: null,
                 requestingData: false,
-                sortOption: '-created_on',
+                sortOption: '-like_count',
                 comment: '',
                 loading: false,
                 currentLikeUsersId: 0,
