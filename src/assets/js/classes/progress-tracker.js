81fa8afa26eb7ca8c155a1aec9331c5d7524d762
Curtis <curtis@drumeo.com>
2019-03-15 13:36:23 -0700

    Small refactors.

--- src/assets/js/classes/progress-tracker.js
+++ src/assets/js/classes/progress-tracker.js
@@ -4,29 +4,56 @@ export default class ProgressTracker {
         this.secondsWatched = 0;
     }
 
-    startTimer(){
+    /**
+     * Start the timer
+     *
+     */
+    start(){
         this.tickInterval = this.progressInterval();
     }
 
-    stopTimer(){
+    /**
+     * Stop the timer
+     *
+     */
+    stop(){
         clearInterval(this.tickInterval);
     }
 
+    /**
+     * Send a navigator beacon with a FormData object containing progress data
+     *
+     * @param {string} endpoint - the endpoint to send data to
+     * @param {string|number} contentId - Content ID you wish to track progress for
+     * @param {string|number} watchPosition - Current watch position of the media you are tracking progress
+     * @param {string|number} totalDuration
+     */
+    send({
+        endpoint = '/media-playback-tracking/media-playback-session',
+        mediaId,
+        watchPosition,
+        totalDuration,
+    }){
+        const data = new FormData();
+
+        data.append('seconds_played', this.secondsWatched);
+        data.append('media_id', mediaId);
+
+        if(watchPosition && totalDuration){
+            data.append('current_second', watchPosition);
+            data.append('media_length_seconds', totalDuration);
+        }
+
+        navigator.sendBeacon(endpoint, data);
+    }
+
+    /**
+     * Interval that increments the secondsWatched property every second
+     *
+     */
     progressInterval(){
         return setInterval( () => {
             this.secondsWatched++;
         }, 1000);
-    }
-
-    sendProgress(endpoint, currentWatchPosition = 0){
-        const data = new FormData();
-
-        data.append('seconds_watched', this.secondsWatched);
-
-        if(currentWatchPosition){
-            data.append('watch_position', currentWatchPosition);
-        }
-
-        navigator.sendBeacon(endpoint, data);
     }
 }
