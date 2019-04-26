b39a4de6689b1cbeb99b7ce22a3d75ecb71b9f0a
Curtis <curtis@drumeo.com>
2019-04-26 15:29:16 -0700

    Add 2 more playback rates

--- src/components/VideoPlayer/_PlayerSettings.vue
+++ src/components/VideoPlayer/_PlayerSettings.vue
@@ -100,7 +100,7 @@
             return {
                 qualitiesDropdown: false,
                 ratesDropdown: false,
-                playbackRates: [0.5, 0.75, 1, 1.25, 1.5],
+                playbackRates: [0.5, 0.75, 1, 1.25, 1.5, 1.75, 2],
             }
         },
         computed: {
