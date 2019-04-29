39c840e0de5fba0dd5bc999a434fd99028b7e661
Curtis <curtis@drumeo.com>
2019-04-29 16:09:07 -0700

    Fix bug with courses

--- src/assets/js/models/course.js
+++ src/assets/js/models/course.js
@@ -10,19 +10,19 @@ export default class CourseModel extends ContentModel {
         this.card.color_title = this.postInstructor;
 
         this.list.column_data = [
-            DataMapper.mapDifficulty(this.post),
+            ContentModel.mapDifficulty(this.post),
             this.postChildLessonCount,
             this.postPublisedOn
         ];
 
         if(this.brand === 'guitareo'){
             this.card.color_title = this.getTypeWithIcon();
-            this.card.grey_title = this.postInstructor + ', ' + DataMapper.mapDifficulty(this.post);
+            this.card.grey_title = this.postInstructor + ', ' + ContentModel.mapDifficulty(this.post);
 
             this.list.color_title = null;
             this.list.column_data = [
                 this.postInstructor,
-                DataMapper.mapDifficulty(this.post),
+                ContentModel.mapDifficulty(this.post),
                 this.postChildLessonCount,
                 this.postPublisedOn
             ]
