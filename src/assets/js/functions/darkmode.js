66fffd0b6354bb37630877c0aa779b4c7e849a9d
Curtis <curtis@drumeo.com>
2019-05-07 11:46:49 -0700

    Add max age to darkmode cookie.

--- src/assets/js/functions/darkmode.js
+++ src/assets/js/functions/darkmode.js
@@ -1,7 +1,7 @@
 export default (function(){
     window.enableExperimentalVuesoraDarkMode = function(){
         document.body.classList.add('dark-mode');
-        document.cookie = 'darkmode=true';
+        document.cookie = 'darkmode=true; max-age=31536000';
     };
 
     window.disableExperimentalVuesoraDarkMode = function(){
