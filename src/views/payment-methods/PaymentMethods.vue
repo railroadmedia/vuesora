3c40af914bd6ee41e7c3e70c8e08b47616ab5c19
Curtis <curtis@drumeo.com>
2019-05-13 14:04:35 -0700

    Add payment methods component

---
+++ src/views/payment-methods/PaymentMethods.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="flex flex-row">
+        <h1 class="heading">Payment Methods</h1>
+    </div>
+</template>
+
+<script>
+    import ThemeClasses from "../../mixins/ThemeClasses";
+
+    const defaultPaymentMethod = () => ({
+        id: 0,
+        attributes: {},
+        relationships: {
+            method: { data: {}},
+            billingAddress: { data: {}},
+            userPaymentMethod: { data: {}},
+        }
+    });
+
+    export default {
+        mixins: [ThemeClasses],
+        name: 'payment-methods',
+        props: {
+            paymentMethods: {
+                type: Array,
+                default: () => []
+            },
+
+
+        },
+        data(){
+            return {
+                editingPaymentMethod: defaultPaymentMethod(),
+            }
+        },
+
+    }
+</script>
---
+++ src/views/payment-methods/index.js
@@ -0,0 +1,10 @@
+import vaComponent from './PaymentMethods.vue';
+
+export default {
+    install(Vue, options){
+        Vue.component(
+            vaComponent.name,
+            vaComponent
+        )
+    }
+}
