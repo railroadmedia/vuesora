2eadd59b385d29f5d4d59fbdbc25fa915ddeaf59
Curtis <curtis@drumeo.com>
2019-05-13 14:02:21 -0700

    Remove toast from generic error handler.

--- src/assets/js/services/_error-handler.js
+++ src/assets/js/services/_error-handler.js
@@ -2,13 +2,4 @@ import Toasts from "../classes/toasts";
 
 export default function(error){
     console.error(error);
-    Toasts.push({
-        icon: 'doh',
-        title: 'An error happened on the server...',
-        themeColor: 'error',
-        message: 'Refresh the page to try once more, if it happens again please let us know using the chat below. ' +
-            '<br><br><span class="font-italic text-grey-3">' +
-            'Reference: <span class="font-bold">' + error.response.status + ' - ' + error.response.statusText +
-            '</span></span>'
-    });
 }
