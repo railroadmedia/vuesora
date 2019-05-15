a08304ab875650693ee916bcce15da71b3f64616
Curtis <curtis@drumeo.com>
2019-05-15 10:56:36 -0700

    Add loading animation component

---
+++ src/components/LoadingAnimation/index.js
@@ -0,0 +1,10 @@
+import LoadingAnimation from './LoadingAnimation';
+
+export default {
+    install(Vue, options){
+        Vue.component(
+            LoadingAnimation.name,
+            LoadingAnimation
+        )
+    }
+}
