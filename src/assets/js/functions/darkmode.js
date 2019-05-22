d2aba0f7600c54a6481f214e53d05a4b92367871
Curtis <curtis@drumeo.com>
2019-05-22 09:16:52 -0700

    Fix cookie path

--- src/assets/js/functions/darkmode.js
+++ src/assets/js/functions/darkmode.js
@@ -1,11 +1,11 @@
 export default (function(){
     window.enableExperimentalVuesoraDarkMode = function(){
         document.body.classList.add('dark-mode');
-        document.cookie = 'darkmode=true; max-age=31536000';
+        document.cookie = 'darkmode=true;path=/;max-age=31536000;';
     };
 
     window.disableExperimentalVuesoraDarkMode = function(){
         document.body.classList.remove('dark-mode');
-        document.cookie = 'darkmode=; expires=0;';
+        document.cookie = 'darkmode=;path=/;expires=0;';
     };
 })();
