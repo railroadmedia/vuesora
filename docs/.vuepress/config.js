97762d969717a7d7188309e4e937eac54e956b79
Curtis <curtis@drumeo.com>
2019-06-12 11:54:31 -0700

    Add Vuepress and new documentation system

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,26 @@
+const {version} = require('../../package.json');
+
+module.exports = {
+    title: `Vuesora v${version}`,
+    dest: 'dist',
+    description: 'A custom front-end framework and Vue component library for the Musora member\'s area',
+    head: [
+        ['link', { rel: 'icon', href: '/logo.png' }]
+    ],
+    evergreen: true,
+    themeConfig: {
+        logo: '/logo.png',
+        nav: [
+            {
+                text: 'Project Setup',
+                link: '/'
+            },
+            {
+                text: 'Layout & Style',
+                link: '/layout/'
+            }
+        ],
+        sidebar: 'auto',
+        lastUpdated: 'Last Updated',
+    }
+};
