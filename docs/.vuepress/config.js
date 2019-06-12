e8294bf3952c025cc0d6a5fa6069f97c68f23273
Curtis <curtis@drumeo.com>
2019-06-12 12:02:46 -0700

    Add proper base url

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,7 @@ const {version} = require('../../package.json');
 
 module.exports = {
     title: `Vuesora v${version}`,
+    base: '/vuesora/',
     description: 'A custom front-end framework and Vue component library for the Musora member\'s area',
     head: [
         ['link', { rel: 'icon', href: '/logo.png' }]
