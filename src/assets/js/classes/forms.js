01210de4d54fdc7359fdda4079bb00522a09af5d
Curtis <curtis@drumeo.com>
2019-06-13 15:17:53 -0700

    Add Eslint config

--- src/assets/js/classes/forms.js
+++ src/assets/js/classes/forms.js
@@ -4,10 +4,10 @@
  */
 
 // import validator from 'validator';
-import flatpickr from "flatpickr";
+import flatpickr from 'flatpickr';
 
 export default class Forms {
-    constructor(form){
+    constructor(form) {
         this.formElement = form;
 
         Forms.removeInputEventListeners();
@@ -20,14 +20,14 @@ export default class Forms {
      *
      * @static
      */
-    static addInputEventListeners(){
+    static addInputEventListeners() {
         const inputs = document.querySelectorAll('input, textarea, select');
 
         Array.from(inputs).forEach(input => {
             input.addEventListener('change', Forms.handleInputChange);
 
             // Add the has-input class if the input came pre filled out
-            if(input.value){
+            if (input.value) {
                 input.classList.add('has-input');
             }
         });
@@ -38,14 +38,14 @@ export default class Forms {
      *
      * @static
      */
-    static addFileInputEventListeners(){
+    static addFileInputEventListeners() {
         const fileInputWraps = document.querySelectorAll('.file-input');
 
-        Array.from(fileInputWraps).forEach(wrap => {
+        Array.from(fileInputWraps).forEach((wrap) => {
             const input = wrap.querySelectorAll('input')[0];
             const label = wrap.querySelectorAll('.file-name')[0];
 
-            input.addEventListener('change', event => {
+            input.addEventListener('change', (event) => {
                 label.innerHTML = event.target.files.item(0).name;
             });
         });
@@ -56,10 +56,10 @@ export default class Forms {
      *
      * @static
      */
-    static removeInputEventListeners(){
+    static removeInputEventListeners() {
         const inputs = document.querySelectorAll('input, textarea, select');
 
-        Array.from(inputs).forEach(input => {
+        Array.from(inputs).forEach((input) => {
             input.removeEventListener('change', Forms.handleInputChange);
         });
     }
@@ -69,21 +69,20 @@ export default class Forms {
      *
      * @static
      */
-    static handleInputChange(event){
+    static handleInputChange(event) {
         const element = event.target;
 
-        if(element.value.length){
+        if (element.value.length) {
             element.classList.add('has-input');
-        }
-        else {
+        } else {
             element.classList.remove('has-input');
         }
 
 
-        if(element.type === 'checkbox'){
+        if (element.type === 'checkbox') {
             const hiddenInput = element.parentElement.querySelector('.hidden-input');
 
-            if(hiddenInput != null){
+            if (hiddenInput != null) {
                 hiddenInput.value = element.checked ? 1 : 0;
             }
         }
@@ -94,41 +93,41 @@ export default class Forms {
      *
      * @static
      */
-    static initializeFlatpickrInputs(){
+    static initializeFlatpickrInputs() {
         const flatpickrInputs = document.querySelectorAll('.flatpickr');
 
         Array.from(flatpickrInputs).forEach(input => {
             // Check for the class .enable-time to enable the time input
-            let enableTime = input.classList.contains('enable-time');
-            let dateFormat = "Y-m-d";
-            let altFormat = "F j, Y";
+            const enableTime = input.classList.contains('enable-time');
+            let dateFormat = 'Y-m-d';
+            let altFormat = 'F j, Y';
 
             // If the time input is enabled the formats need to change
-            if(enableTime){
-                dateFormat = "Y-m-d H:i:s";
-                altFormat = "F j, Y \\a\\t h:i"
+            if (enableTime) {
+                dateFormat = 'Y-m-d H:i:s';
+                altFormat = 'F j, Y \\a\\t h:i';
             }
 
             flatpickr(input, {
-                enableTime: enableTime,
+                enableTime,
                 altInput: true,
-                altFormat: altFormat,
-                dateFormat: dateFormat,
+                altFormat,
+                dateFormat,
             });
         });
     }
 
-    static checkErrorsInModalForms(){
+    static checkErrorsInModalForms() {
         const erroredInputs = document.querySelectorAll('input.has-error, select.has-error');
 
-        if(erroredInputs.length > 0){
+        if (erroredInputs.length > 0) {
             let parentModal = erroredInputs[0].parentElement;
 
-            while(parentModal != null && !parentModal.classList.contains('modal')){
+            while (parentModal != null && !parentModal.classList.contains('modal')) {
                 parentModal = parentModal.parentElement;
             }
 
-            if(parentModal){
+            if (parentModal) {
                 parentModal.classList.add('active');
                 document.body.classList.add('no-scroll');
                 window.appendBackgroundOverlay();
