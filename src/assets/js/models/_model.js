fe0b7469e72498d7271bf4488371739a7bca0f95
Curtis <curtis@drumeo.com>
2019-06-17 13:45:25 -0700

    Linting

--- src/assets/js/models/_model.js
+++ src/assets/js/models/_model.js
@@ -32,31 +32,31 @@ import ContentModel from './_default';
 export default class ContentTypeModel {
     constructor(type, {
         brand,
-        post
-    }){
-        switch(type){
-            case 'chord-and-scale':         return new ChordsAndScalesModel({brand, post});
-            case 'course':                  return new CourseModel({brand, post});
-            case 'course-part':             return new CoursePartModel({brand, post});
-            case 'learning-path':           return new LearningPathModel({brand, post});
-            case 'learning-path-part':      return new LearningPathPartModel({brand, post});
-            case 'pack-bundle':             return new PackBundleModel({brand, post});
-            case 'pack-bundle-lesson':      return new PackBundleLessonModel({brand, post});
-            case 'play-along':              return new PlayAlongModel({brand, post});
-            case 'play-along-part':         return new PlayAlongPartModel({brand, post});
-            case 'recording':               return new RecordingModel({brand, post});
-            case 'rudiment':                return new RudimentModel({brand, post});
-            case 'semester-pack':           return new SemesterPackModel({brand, post});
-            case 'semester-pack-lesson':    return new SemesterPackLessonModel({brand, post});
-            case 'show':                    return new ShowModel({brand, post});
-            case 'song':                    return new SongModel({brand, post});
-            case 'song-part':               return new SongPartModel({brand, post});
-            case 'student-focus':           return new StudentFocusModel({brand, post});
-            case 'student-review':          return new StudentReviewModel({brand, post});
-            case 'question-and-answer':     return new QnaModel({brand, post});
-            case 'unit':                    return new UnitModel({brand, post});
-            case 'unit-part':               return new UnitPartModel({brand, post});
-            default:                        return new ContentModel({brand, post});
+        post,
+    }) {
+        switch (type) {
+        case 'chord-and-scale': return new ChordsAndScalesModel({ brand, post });
+        case 'course': return new CourseModel({ brand, post });
+        case 'course-part': return new CoursePartModel({ brand, post });
+        case 'learning-path': return new LearningPathModel({ brand, post });
+        case 'learning-path-part': return new LearningPathPartModel({ brand, post });
+        case 'pack-bundle': return new PackBundleModel({ brand, post });
+        case 'pack-bundle-lesson': return new PackBundleLessonModel({ brand, post });
+        case 'play-along': return new PlayAlongModel({ brand, post });
+        case 'play-along-part': return new PlayAlongPartModel({ brand, post });
+        case 'recording': return new RecordingModel({ brand, post });
+        case 'rudiment': return new RudimentModel({ brand, post });
+        case 'semester-pack': return new SemesterPackModel({ brand, post });
+        case 'semester-pack-lesson': return new SemesterPackLessonModel({ brand, post });
+        case 'show': return new ShowModel({ brand, post });
+        case 'song': return new SongModel({ brand, post });
+        case 'song-part': return new SongPartModel({ brand, post });
+        case 'student-focus': return new StudentFocusModel({ brand, post });
+        case 'student-review': return new StudentReviewModel({ brand, post });
+        case 'question-and-answer': return new QnaModel({ brand, post });
+        case 'unit': return new UnitModel({ brand, post });
+        case 'unit-part': return new UnitPartModel({ brand, post });
+        default: return new ContentModel({ brand, post });
         }
     }
 }
