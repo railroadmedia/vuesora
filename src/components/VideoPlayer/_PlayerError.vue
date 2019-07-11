288b91a7131e11c5df42b5b66faed84b31b33378
Curtis <curtis@drumeo.com>
2019-07-11 11:05:39 -0700

    Move components to their own SFC

---
+++ src/components/VideoPlayer/_PlayerError.vue
@@ -0,0 +1,19 @@
+<template>
+    <div
+        class="player-error bg-error text-white corners-3 shadow pa-1"
+    >
+        <p class="body font-bold mb-2">
+            No compatible source was found for this video.
+        </p>
+        <p class="tiny">
+            If the issue persists, please contact support using the chat
+            widget on the bottom right of your screen.
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PlayerError',
+};
+</script>
