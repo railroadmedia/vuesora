abb95f1ed73b4055bf2efca7ac4091d3d4c5b7fe
Curtis <curtis@drumeo.com>
2019-08-13 14:17:49 -0700

    Fix publish script to now make extra commits

--- publish.sh
+++ publish.sh
@@ -11,11 +11,7 @@ if git diff-index --quiet HEAD --; then
     PACKAGE_VERSION=$(grep -m1 version package.json | awk -F: '{ print $2 }' | sed 's/[", ]//g')
 
     echo 'Creating Tag and Commit for Vuesora Version: ' $PACKAGE_VERSION
-
-    # Create a tag and commit for the patch and push it
-    git tag $PACKAGE_VERSION;
-    git add --all
-    git commit -am $PACKAGE_VERSION;
+    
     git push --tags;
     git push;
 
