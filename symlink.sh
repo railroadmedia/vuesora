80f7be338b03daba0c70551f6b5caf42b85256f2
calebfavor <calebfavor@gmail.com>
2019-08-28 11:43:48 -0700

    Fixing cart address update endpoint keys. Adding node-sass rebuild to symlink command to fix errors.

--- symlink.sh
+++ symlink.sh
@@ -8,17 +8,17 @@ do
     case $opt in
         "drumeo")
             echo "Symlinking Vuesora with Drumeo!"
-            npm run lib:watch & npm link; cd /app/drumeo/laravel; npm link vuesora; npm run watch;
+            npm run lib:watch & npm link; cd /app/drumeo/laravel; npm rebuild node-sass; npm link vuesora; npm run watch;
             echo "Watching Drumeo For Changes..."
             ;;
         "pianote")
             echo "Symlinking Vuesora with Pianote!"
-            npm run lib:watch & npm link; cd /app/pianote; npm link vuesora; npm run watch;
+            npm run lib:watch & npm link; cd /app/pianote; npm rebuild node-sass; npm link vuesora; npm run watch;
             echo "Watching Pianote For Changes..."
             ;;
         "guitareo")
             echo "Symlinking Vuesora with Guitareo!"
-            npm run lib:watch & npm link; cd /app/guitareo; npm link vuesora; npm run watch;
+            npm run lib:watch & npm link; cd /app/guitareo; npm rebuild node-sass; npm link vuesora; npm run watch;
             ;;
         *)
           echo "Invalid option $REPLY"
