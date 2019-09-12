68ec08b60c989c0210637b41aad343c5795855f3
Curtis <curtis@drumeo.com>
2019-09-12 11:02:22 -0700

    Add conditions to render a simple version for the best book page.

--- src/views/play-alongs/PlayAlongs.vue
+++ src/views/play-alongs/PlayAlongs.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="flex flex-column grow play-alongs">
         <play-alongs-filters
+            v-if="showFilters"
             :theme-color="themeColor"
             :filter-options="filterOptions"
             :selected-filters="selectedFilters"
@@ -8,7 +9,10 @@
             @filterChange="handleFilterChange"
         ></play-alongs-filters>
 
-        <div class="flex flex-row flex-wrap-xs-only">
+        <div
+            v-if="showFilters"
+            class="flex flex-row flex-wrap-xs-only"
+        >
             <div class="flex flex-column flex-auto">
                 <div class="flex flex-row form-group align-v-center pa-2">
                     <span class="toggle-input mr-1">
@@ -62,7 +66,10 @@
             </div>
         </div>
 
-        <div class="flex flex-row bg-grey-7 bt-grey-1-1 pagination-row align-h-right">
+        <div
+            v-if="showPagination"
+            class="flex flex-row bg-grey-7 bt-grey-1-1 pagination-row align-h-right"
+        >
             <pagination
                 :current-page="Number(page)"
                 :total-pages="totalPages"
@@ -85,7 +92,10 @@
             @click.native="updateTrack(item)"
         ></play-alongs-list-item>
 
-        <div class="flex flex-row bg-grey-7 bt-grey-1-1 pagination-row align-h-right">
+        <div
+            v-if="showPagination"
+            class="flex flex-row bg-grey-7 bt-grey-1-1 pagination-row align-h-right"
+        >
             <pagination
                 :current-page="Number(page)"
                 :total-pages="totalPages"
@@ -171,11 +181,6 @@ export default {
             default: () => ({}),
         },
 
-        limit: {
-            type: Number,
-            default: 20,
-        },
-
         userId: {
             type: [String, Number],
             default: () => null,
@@ -185,6 +190,21 @@ export default {
             type: String,
             default: () => '/railcontent/content',
         },
+
+        showFilters: {
+            type: Boolean,
+            default: () => true,
+        },
+
+        showPagination: {
+            type: Boolean,
+            default: () => true,
+        },
+
+        useUrlParams: {
+            type: Boolean,
+            default: () => true,
+        },
     },
     data() {
         return {
@@ -192,7 +212,8 @@ export default {
             loading: false,
             currentPlaybackRate: 1,
             currentVolume: 1,
-            page: this.preLoadedContent.meta.page,
+            page: 1,
+            limit: 20,
             totalResults: this.preLoadedContent.meta.totalResults,
             filterOptions: this.preLoadedContent.meta.filterOptions,
             isKeyboardControlsEnabled: false,
@@ -243,7 +264,7 @@ export default {
 
         filterQueryObject() {
             return {
-                included_fields: Object.keys(this.selectedFilters)
+                required_fields: Object.keys(this.selectedFilters)
                     .filter(key => this.selectedFilters[key] != null)
                     .map(key => `${key},${this.selectedFilters[key]}`),
                 page: this.page,
@@ -269,6 +290,10 @@ export default {
                 this.eventHandlers[event],
             );
         });
+
+        if (this.useUrlParams) {
+            this.getActiveFilters();
+        }
 
         this.getDefaultVolume();
 
@@ -303,6 +328,7 @@ export default {
                         this.content = response.data.data;
                         this.page = response.data.meta.page;
                         this.totalResults = response.data.meta.totalResults;
+                        this.filterOptions = response.data.meta.filterOptions;
 
                         this.$nextTick(() => {
                             this.loading = false;
@@ -311,6 +337,21 @@ export default {
 
                     this.preFetchedContent = null;
                 });
+        },
+
+        getActiveFilters() {
+            const urlParams = QueryString.parse(window.location.search, { arrayFormat: 'bracket' });
+
+            this.page = urlParams.page || 1;
+            this.limit = urlParams.limit || 20;
+
+            if (urlParams.required_fields && urlParams.required_fields.length > 0) {
+                urlParams.required_fields.forEach((field) => {
+                    const keyValue = field.split(',');
+
+                    this.$set(this.selectedFilters, keyValue[0], keyValue[1]);
+                });
+            }
         },
 
         updateTrack(item) {
@@ -411,7 +452,9 @@ export default {
                         ? 1
                         : Number(this.page) + 1;
 
-                    await this.handlePageChange({ page: pageToGoTo });
+                    if (pageToGoTo !== this.page) {
+                        await this.handlePageChange({ page: pageToGoTo });
+                    }
 
                     // eslint-disable-next-line prefer-destructuring
                     contentToPlay = this.content[0];
@@ -448,7 +491,9 @@ export default {
             this.audioPlayer.volume = volume / 100;
             this.currentVolume = this.audioPlayer.volume;
 
-            localStorage.setItem('playAlongsVolume', volume);
+            if (volume !== 0) {
+                localStorage.setItem('playAlongsVolume', volume);
+            }
         },
 
         toggleDrums() {
@@ -492,6 +537,8 @@ export default {
             if (this.anchorMouseDown.a) {
                 if (mousePositionOffset >= this.anchorOffsets.b - 1) {
                     mousePositionOffset = this.anchorOffsets.b - 1;
+                } else if (mousePositionOffset < 0) {
+                    mousePositionOffset = 0;
                 }
 
                 this.$set(this.anchorOffsets, 'a', mousePositionOffset);
@@ -500,6 +547,8 @@ export default {
             if (this.anchorMouseDown.b) {
                 if (mousePositionOffset <= this.anchorOffsets.a + 1) {
                     mousePositionOffset = this.anchorOffsets.a + 1;
+                } else if (mousePositionOffset > 100) {
+                    mousePositionOffset = 100;
                 }
 
                 this.$set(this.anchorOffsets, 'b', mousePositionOffset);
@@ -525,7 +574,9 @@ export default {
             this.page = 1;
             this.$set(this.selectedFilters, payload.key, payload.value);
 
-            this.updatePageUrl();
+            if (this.useUrlParams) {
+                this.updatePageUrl();
+            }
 
             this.$nextTick(() => {
                 this.playedContent = [];
@@ -537,7 +588,10 @@ export default {
         handlePageChange({ page }) {
             this.page = page;
 
-            this.updatePageUrl();
+            if (this.useUrlParams) {
+                this.updatePageUrl();
+            }
+
             return this.getContent();
         },
 
@@ -565,9 +619,7 @@ export default {
         
         getDefaultVolume() {
             if (window.localStorage.getItem('playAlongsVolume') != null) {
-                this.changeVolume({
-                    volume: Number(window.localStorage.getItem('playAlongsVolume')),
-                });
+                this.changeVolume(Number(window.localStorage.getItem('playAlongsVolume')));
             }
         },
 
