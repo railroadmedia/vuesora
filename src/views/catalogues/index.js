426964c2fef590c409bf86cb00af433f4193c37b
Miguel Fondeur <miguelfondeur@gmail.com>
2021-11-16 03:42:19 +0000

    update coach events component

--- src/views/catalogues/index.js
+++ src/views/catalogues/index.js
@@ -1,11 +1,13 @@
 import Vue from 'vue';
 import ContentCatalogue from './ContentCatalogue';
+import CoachesCatalogue from './CoachesCatalogue';
 
 export default {
     install(Vue, options) {
         Vue.component(
             ContentCatalogue.name,
             ContentCatalogue,
+            CoachesCataglogue,
         );
     },
 };
