e4978ebe46d13078d6dfcad7df2221ce06059ecb
Miguel Fondeur <miguelfondeur@gmail.com>
2021-11-22 21:21:15 +0000

    update mobile contact form

--- src/components/ContactEmailForm/ContactEmailForm.vue
+++ src/components/ContactEmailForm/ContactEmailForm.vue
@@ -69,8 +69,8 @@
             placeholder="Add message here..."
         />
 
-        <div class="tw-flex tw-full">
-            <div class="tw-w-1/2">
+        <div class="tw-flex tw-flex-col-reverse md:tw-flex-row tw-full">
+            <div class="tw-my-4 tw-inline-flex tw-mx-auto md:tw-my-0 md:tw-w-1/2">
                 <!-- Recaptcha -->
                 <recaptcha :siteKey="captchakey"
                            @update:verified="formVerified = $event"    
