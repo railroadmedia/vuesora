38c40ce79586be43bec1f96a69e37d6a71ce6019
Yalung Tang <ytfreak@gmail.com>
2021-11-29 15:12:39 -0300

    carcass

---
+++ src/views/catalogues/CoachGridCatalogueContainer.vue
@@ -0,0 +1,14 @@
+<template>
+    <div
+        class="flex flex-row align-v-top test-coach"
+        :class="[noWrap ? 'overflow' : 'flex-wrap', {'nmh-1': !displayInline}]"
+    >
+
+    </div>
+</template>
+<script>
+
+export default {
+    name: 'ContentCatalogueContainer',
+};
+</script>
