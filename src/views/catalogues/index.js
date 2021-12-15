bc8703b9464ae27caf9db45d67fa6154a76f0d50
Miguel Fondeur <miguelfondeur@gmail.com>
2021-12-15 17:28:07 +0000

    delete unused  coaches catalogue component

--- src/views/catalogues/index.js
+++ src/views/catalogues/index.js
@@ -1,15 +1,12 @@
 import Vue from 'vue';
 import ContentCatalogue from './ContentCatalogue';
 import ContentCatalogueContainer from './ContentCatalogueContainer';
-import CoachesCatalogue from './CoachesCatalogue';
 
 export default {
     install(Vue, options) {
         Vue.component(
             ContentCatalogue.name,
             ContentCatalogue,
-            CoachesCatalogue.name,
-            CoachesCatalogue,
             ContentCatalogueContainer.name,
             ContentCatalogueContainer,
         );
