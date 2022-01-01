1e1b41510c7bb57a139f775ce216ef65c0834946
calebfavor <calebfavor@gmail.com>
2021-12-31 16:56:58 -0800

    Fixing coach event more

--- src/components/Coaches/CoachEvent.vue
+++ src/components/Coaches/CoachEvent.vue
@@ -13,7 +13,7 @@
                 </div>
             </a>
 
-            <div class="tw-flex tw-w-full tw-flex-row">
+            <div class="tw-flex tw-w-full tw-flex-col sm:tw-flex-row">
                 <!-- Event Details -->
                 <div class="tw-flex tw-flex-col tw-justify-center tw-pr-4">
                     
@@ -66,7 +66,7 @@
                 </div>
 
                 <!-- Buttons -->
-                <div class="tw-flex tw-flex-col tw-justify-center tw-mt-3 sm:tw-mt-0 tw-ml-auto">
+                <div class="tw-flex tw-flex-col tw-justify-center tw-mt-3 sm:tw-mt-0 sm:tw-ml-auto tw-mr-auto">
                     <div class="tw-mb-3" v-if="eventIsLive || showWatch">
                         <div class="tw-flex tw-flex-row tw-flex-wrap-md tw-mt-2">
                             <div>
