aee50f4fc9a5bd47be2cb7189e7629e68774892e
calebfavor <calebfavor@gmail.com>
2022-07-06 15:28:47 -0700

    Changing column data from LESSONS to COURSES for child count

--- src/assets/js/models/pack.js
+++ src/assets/js/models/pack.js
@@ -7,8 +7,9 @@ export default class PackModel extends ContentModel {
             post,
         });
 
+        // this is only for pianote classical method pack which is show in the list view on pianote
         this.list.column_data = [
-            this.postChildLessonCount,
+            `${this.post.lesson_count} COURSES`,
             `${this.post.total_xp} xp`,
         ];
     }
